3c693165f25be0ddfd620c2cc512ac1707fbbb43
cds0987 <ttnguyen1410@gmail.com>
2026-03-11 22:29:43 +0700

    Change base class encode model

--- encode/model.py
+++ encode/model.py
@@ -1,9 +1,12 @@
-class encodeModel:
+from abc import ABC, abstractmethod
+
+class encodeModel(ABC):
     def __init__(self, modelname,*args, **kwargs):
         """
         model: model name or model instance
         kwargs: additional args passed to model loading function
         """
         self.modelname = modelname
+    @abstractmethod
     def encode(self,*args, **kwargs):
         pass
