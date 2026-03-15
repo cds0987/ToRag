7298658f8448c494b7a487b7c411b0dbcbc5ecc5
cds0987 <ttnguyen1410@gmail.com>
2026-03-15 23:46:00 +0700

    init jin AI

--- loadModel/__init__.py
+++ loadModel/__init__.py
@@ -1,3 +1,4 @@
 from .load import loader_manager
 from .sentencetransformer_loader import sentencetransformerLoader
 from .llamacpp_loader import llamacppLoader
+from .jinAImodel import jinAImodelLoader
