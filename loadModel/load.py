8a26a294a033c898dde6f726c39d21ab2a59c0a5
cds0987 <ttnguyen1410@gmail.com>
2026-03-15 23:53:31 +0700

    Fix load model

--- loadModel/load.py
+++ loadModel/load.py
@@ -2,23 +2,28 @@ from pprint import pprint
 class ModelLoaderManager:
 
     def __init__(self):
-        self.ebdloaders = {}
+        self.model = {}
 
     def register_encode(self, loader, name=None):
+        if self.model.get("encode") is None:
+            self.model["encode"] = {}
         if name:
-            self.ebdloaders[name] = loader
+            self.model["encode"][name] = loader
         else:
             # Use the loader's class name as the default name
-            self.ebdloaders[loader.__class__.__name__.lower()] = loader
+            self.model["encode"][loader.__class__.__name__.lower()] = loader
 
     def loadModel(self,**kwargs):
         print("\n=== Model Configuration ===")
         pprint(kwargs)
         print("=============================\n")
+        loadtype = kwargs.get("loadtype", "encode")
+        #pop the loadtype from kwargs to avoid passing it to the loaders
+        kwargs.pop("loadtype", None)
         modeltype = kwargs.get('modeltype')
         #pop the modeltype from kwargs to avoid passing it to the loaders
         kwargs.pop('modeltype', None)
-        loader = self.ebdloaders.get(modeltype)
+        loader = self.model[loadtype].get(modeltype)
         if loader:
             return loader.load(**kwargs)
         return None
