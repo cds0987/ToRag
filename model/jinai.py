300d0d55f3264584bc28dcf5ff90e02a08bf67e7
cds0987 <ttnguyen1410@gmail.com>
2026-03-16 08:11:57 +0700

    ncols=80,

--- model/jinai.py
+++ model/jinai.py
@@ -53,7 +53,7 @@ class jinaitokenencode(BaseModel):
                     desc="Encoding",
                     unit="batch",
                     colour="green",
-                    dynamic_ncols=True,
+                    ncols=80,
                     total=(len(texts) + batch_size - 1) // batch_size
                 )
 
